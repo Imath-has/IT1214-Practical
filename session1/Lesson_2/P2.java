3b3576f02b962f0bf9b8557a1705cf71129ef916
Imath-has <Imathhasan23@gmail.com>
2025-05-01 23:04:30 +0530

    Add files via upload

---
+++ session1/Lesson_2/P2.java
@@ -0,0 +1,14 @@
+// this is a comment 
+//this is our first java program
+
+import java.io.*; //this is how we import packages
+
+class MySecondClass{ // class name should start with capital letters
+
+      public static void main(String[] ar) {
+		  System.out.println("The first argument is "+ar[0]);
+		  System.out.println("The second argument is "+ar[1]);
+		  System.out.println("The third argument is "+ar[2]);
+		  System.out.println(ar[3]+" "+ar[5]+" "+ar[4]);
+	  }
+}
